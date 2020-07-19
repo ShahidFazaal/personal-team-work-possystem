4b8516b6def283e45815170726404c2706653a6e
Waruna Lakshan <warulakshan96@gmail.com>
2020-07-19 16:03:54 +0530

    Update dashBoardController.java class and DashBoard.fxml file

--- src/controller/DashBoardController.java
+++ src/controller/DashBoardController.java
@@ -5,14 +5,15 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import util.CustomerTM;
 import util.DBConnection;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DashBoardController {
     public JFXTextField txt_Customer_Name;
@@ -23,40 +24,57 @@ public class DashBoardController {
     public JFXButton btn_Save_Customer;
     public JFXButton btn_Delete_Customer;
     public TableView<CustomerTM>tbl_Customer_details;
+    public JFXTextField txt_Customer_ID;
 
-    public void btn_ADDnew_Customer(ActionEvent actionEvent) {
+    public void initialize(){
         tbl_Customer_details.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("customerId"));
         tbl_Customer_details.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("customerName"));
         tbl_Customer_details.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("customerAddress"));
         tbl_Customer_details.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("customerContact"));
 
+        LoadCustomerInDatabase();
+    }
+    public void btn_ADDnew_Customer(ActionEvent actionEvent) {
 
-                }
+    }
 
 
-    public void btn_Save_onAction_Customer(ActionEvent actionEvent) {
+    public void btn_Save_onAction_Customer(ActionEvent actionEvent) throws SQLException {
+        int customerId = Integer.parseInt(txt_Customer_ID.getText());
+        String customerName = txt_Customer_Name.getText();
+        String customerAddress = txt_Customer_Address.getText();
+        int customerContact = Integer.parseInt(txt_Customer_Contact.getText());
+
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customers VALUES (?,?,?,?)");
+            preparedStatement.setObject(1, customerId);
+            preparedStatement.setObject(2, customerName);
+            preparedStatement.setObject(3, customerAddress);
+            preparedStatement.setObject(4, customerContact);
+            preparedStatement.executeUpdate();
+            new Alert(Alert.AlertType.INFORMATION, "Successfully Added", ButtonType.OK).showAndWait();
+            LoadCustomerInDatabase();
+        LoadCustomerInDatabase();
+    }
+
+    public void btn_Delete_OnAction_Customer(ActionEvent actionEvent) {
+    }
+    private void LoadCustomerInDatabase() {
         try {
-            PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement("select customerId from customers order by customerId desc limit 1");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (!resultSet.next()) {
-                lbl_Customer_ID.setText("0001");
-            } else {
-                String lastID = resultSet.getString(1);
-                String substring = lastID.substring(1, 4);
-                int newID = Integer.parseInt(substring) + 1;
-                if (newID < 10) {
-                    lbl_Customer_ID.setText("000" + newID);
-                } else if (newID < 100) {
-                    lbl_Customer_ID.setText("00" + newID);
-                } else {
-                    lbl_Customer_ID.setText("0" + newID);
-                }
+            Statement statement = DBConnection.getInstance().getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from customers");
+            tbl_Customer_details.getItems().clear();
+            while (resultSet.next()) {
+               int customerId = resultSet.getInt(1);
+                String customerName = resultSet.getString(2);
+                String customerAddress = resultSet.getString(3);
+                int customerContact = resultSet.getInt(4);
+
+                CustomerTM customer = new CustomerTM(customerId, customerName, customerAddress, customerContact);
+                tbl_Customer_details.getItems().add(customer);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public void btn_Delete_OnAction_Customer(ActionEvent actionEvent) {
     }
 }
